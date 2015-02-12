279e09751dcd115fb65f66581c23cac6daf6fc0c
Brock Norton <bbrocknorton@gmail.com>
2015-02-11 19:09:22 -0700

    Show Hide Toggle on Form...its like jQuery's Show hide capability :)

---
+++ controller.js
@@ -0,0 +1,11 @@
+var app = angular.module('show-hide');
+
+app.controller('MainController', function($scope) {
+	$scope.toggle = function() {
+		if ($scope.showForm) {
+			$scope.showForm = false;
+		} else {
+			$scope.showForm = true;
+		}
+	}
+});
